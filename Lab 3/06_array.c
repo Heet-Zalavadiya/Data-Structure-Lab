b4f09dda2911feddac132e1d9a1c7a9660751683
Heet-Zalavadiya <heetzalavadia@gmail.com>
2025-06-11 22:29:05 +0530

    Lab-3 Update

--- Lab 3/06_array.c
+++ Lab 3/06_array.c
@@ -17,7 +17,7 @@ int main()
             index = i;
             break;
         }
-        }
+    }
     arr[index] = m;
 
     for (int i = 0; i < 10; i++)
--- Lab 3/07_array.c
+++ Lab 3/07_array.c
@@ -2,19 +2,42 @@
 
 int main()
 {
+    // int n = 5;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n - 1 - i; j++)
+    //     {
+    //         printf(" ");
+    //     }
+    //     int value = 1;
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         printf("%d ", value);
+    //         value = value * (i - j) / (j + 1);
+    //     }
+    //     printf("\n");
+    // }
+
     int n = 5;
+    int pascal[5][5] = {0};
+
+    // Build Pascal's Triangle
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - 1 - i; j++)
+        pascal[i][0] = 1;
+        for (int j = 1; j <= i; j++)
         {
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+        }
+    }
+
+    // Print Pascal's Triangle
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < n - i - 1; k++)
             printf(" ");
-        }
-        int value = 1;
         for (int j = 0; j <= i; j++)
-        {
-            printf("%d ", value);
-            value = value * (i - j) / (j + 1);
-        }
+            printf("%d ", pascal[i][j]);
         printf("\n");
     }
 
